03a37167e38c524b650626490c9e467c9414989a
sinyoung0110 <dpqlc7008@naver.com>
2025-08-11 15:44:54 +0900

    망할 https- cors에러

--- webtoon-dashboard/backend/main.py
+++ webtoon-dashboard/backend/main.py
@@ -24,9 +24,11 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost:8080",
         "http://localhost:3000",
         "https://webtoon-analytics-dashboard.vercel.app",
-        "https://localhost:8080"
+        "http://127.0.0.1:8080",     # 👈 있으면 편함
+        "http://127.0.0.1:3000",
 
     ],
     allow_credentials=True,
